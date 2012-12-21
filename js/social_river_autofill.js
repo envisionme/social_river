0d8d260dfa0b2f262da004b9706d33ffbdae2dff
Ivan Breet <ivanbreet@gmail.com>
2012-12-21 17:32:46 +0000

    Fixed: Better JS file handling to make it script loader compatible

---
+++ js/social_river_autofill.js
@@ -0,0 +1,206 @@
+Drupal.behaviors.social_river_autofill = function(context) {
+  $("#edit-social-river-user-settings-bitbucket").autofill({
+    value: "Bitbucket username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-bitly").autofill({
+    value: "Bitly username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-blogger").autofill({
+    value: "Blogger username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-dailymotion").autofill({
+    value: "Dailymotion username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-delicious").autofill({
+    value: "Delicious username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-deviantart").autofill({
+    value: "Deviantart username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-dribbble").autofill({
+    value: "Dribbble username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-facebook").autofill({
+    value: "Facebook page ID",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-flickr").autofill({
+    value: "Flickr ID",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-foursquare").autofill({
+    value: "Foursquare feed ID",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-foomark").autofill({
+    value: "Foomark username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-formspring").autofill({
+    value: "Formspring username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-forrst").autofill({
+    value: "Forrst username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-gimmebar").autofill({
+    value: "Gimmebar username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-github").autofill({
+    value: "Github username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-googleplus").autofill({
+    value: "Googleplus username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-googlereader").autofill({
+    value: "Googlereader feed ID",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-instapaper").autofill({
+    value: "Instapaper username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-iusethis").autofill({
+    value: "Iusethis username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-lastfm").autofill({
+    value: "Lastfm username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-mlkshk").autofill({
+    value: "Mlkshk username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-picplz").autofill({
+    value: "Picplz username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-pinboard").autofill({
+    value: "Pinboard username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-posterous").autofill({
+    value: "Posterous username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-reddit").autofill({
+    value: "Reddit username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-slideshare").autofill({
+    value: "Slideshare username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-snipplr").autofill({
+    value: "Snipplr username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-stackoverflow").autofill({
+    value: "Stackoverflow username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-tumblr").autofill({
+    value: "Tumblr username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-twitter").autofill({
+    value: "Twitter username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-vimeo").autofill({
+    value: "Vimeo username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-wordpress").autofill({
+    value: "Wordpress username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-youtube").autofill({
+    value: "Youtube username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+  $("#edit-social-river-user-settings-zotero").autofill({
+    value: "Zotero username",
+    prePopulate: "",
+    defaultTextColor: "#ccc",
+    activeTextColor: "#333"
+  });
+}
